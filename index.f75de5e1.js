e48854919c79da55a6b96c6ac827c86b007a319d
Kyrylo Golikov <pipecc1985@gmail.com>
2024-12-20 16:14:49 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,14 @@
+'use strict';
+const population = document.querySelectorAll('.population');
+const populationArray = [
+    ...population
+].map((item)=>parseInt(item.textContent.replace(/,/g, ''), 10)); // eslint-disable-line
+const totalList = document.querySelector('.total-population');
+const averageList = document.querySelector('.average-population');
+const total = populationArray.reduce((sum, value)=>sum + value, 0);
+const average = Math.round(total / populationArray.length);
+const formatNumber = (number)=>number.toLocaleString('en-US');
+totalList.textContent = formatNumber(total);
+averageList.textContent = formatNumber(average);
+
+//# sourceMappingURL=index.f75de5e1.js.map
